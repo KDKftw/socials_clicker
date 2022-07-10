5c49cef804f6be25f5d311d7a49f32a858ceb7da
Ondra <60475700+KDKftw@users.noreply.github.com>
2022-07-10 21:28:44 +0200

    Create FL_twitter.py

---
+++ venv/FL_twitter.py
@@ -0,0 +1,26 @@
+import pyautogui as p
+import time
+
+#view1Location = [785, 496]
+#subscribeLocation = [903, 330]
+#closeWindowLocation = [981, 14]
+#resfreshLocation = [78, 58]
+
+view1Location = [549, 497]
+followLocation = [736, 340]
+closeWindowLocation = [984, 9]
+confirmLocation =  [816, 567]
+resfreshLocation = [55.49375, 41.24444444444445]
+
+while True:
+    time.sleep(3)
+    p.click(view1Location)
+    time.sleep(6)
+    p.click(followLocation)
+    time.sleep(4)
+    p.click(closeWindowLocation)
+    time.sleep(4)
+    p.click(confirmLocation)
+    time.sleep(8)
+    p.click(resfreshLocation)
+    time.sleep(3)
