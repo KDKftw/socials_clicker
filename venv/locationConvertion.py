98d6ed5c7b0a4c2ec9e68b0a97fa29dd2e3e713c
Ondra <ooo.kadoun@gmail.com>
2022-07-10 17:28:56 +0200

    cords converter

--- venv/locationConvertion.py
+++ venv/locationConvertion.py
@@ -1,3 +1,20 @@
-
-#NTB = 1366x768
+# NTB = 1366x768
 ##PC = 1920x1080
+
+oldResolution = [1920, 1080]
+newResolution = [1366, 768]
+
+view1Location = [785, 496]
+
+
+def newCoordsFnc(oldCoords):
+    newCoords = []
+    newCoordsX = oldCoords[0] / oldResolution[0] * newResolution[0]
+    newCoords.append(newCoordsX)
+    newCoordsY = oldCoords[1] / oldResolution[1] * newResolution[1]
+    newCoords.append(newCoordsY)
+    print(newCoords)
+    return newCoords
+
+
+newCoordsFnc(view1Location)
