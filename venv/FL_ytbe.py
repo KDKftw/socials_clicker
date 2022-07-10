f5f0da54f02a3322cd66404381b42d62402bc4bb
Ondra <ooo.kadoun@gmail.com>
2022-07-10 18:14:31 +0200

    no message

--- venv/FL_ytbe.py
+++ venv/FL_ytbe.py
@@ -4,10 +4,17 @@ import time
 view1Location = [785, 496]
 subscribeLocation = [903, 330]
 closeWindowLocation = [981, 14]
+resfreshLocation = [78, 58]
 
-time.sleep(3)
-p.click(view1Location)
-time.sleep(6)
-p.click(subscribeLocation)
-time.sleep(3)
-p.click(closeWindowLocation)
+
+
+while True:
+    time.sleep(3)
+    p.click(view1Location)
+    time.sleep(6)
+    p.click(subscribeLocation)
+    time.sleep(4)
+    p.click(closeWindowLocation)
+    time.sleep(12)
+    p.click(resfreshLocation)
+    time.sleep(3)
