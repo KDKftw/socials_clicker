57c0501a96d4319db1c1636e0e7fc086ca649b9c
Ondra <60475700+KDKftw@users.noreply.github.com>
2022-07-10 19:19:14 +0200

    Update FL_ytbe.py

--- venv/FL_ytbe.py
+++ venv/FL_ytbe.py
@@ -6,9 +6,9 @@ import time
 #closeWindowLocation = [981, 14]
 #resfreshLocation = [78, 58]
 
-view1Location = [558.4947916666667, 352.7111111111111]
-subscribeLocation = [642.446875, 234.66666666666669]
-closeWindowLocation = [697.9406250000001, 9.955555555555556]
+view1Location = [549, 497]
+subscribeLocation = [906, 332]
+closeWindowLocation = [995, 24]
 resfreshLocation = [55.49375, 41.24444444444445]
 
 while True:
