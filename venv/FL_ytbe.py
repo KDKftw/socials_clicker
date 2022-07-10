a77f7392b1443075fabbfe01faff6f1b9ba39ee8
Ondra <ooo.kadoun@gmail.com>
2022-07-10 17:03:13 +0200

    no message

--- venv/FL_ytbe.py
+++ venv/FL_ytbe.py
@@ -2,8 +2,12 @@ import pyautogui as p
 import time
 
 view1Location = [785, 496]
-
-
+subscribeLocation = [903, 330]
+closeWindowLocation = [981, 14]
 
 time.sleep(3)
 p.click(view1Location)
+time.sleep(6)
+p.click(subscribeLocation)
+time.sleep(3)
+p.click(closeWindowLocation)
