dbf2c953df3ac77e399f1daea596cd428ae597f8
Ondra <ooo.kadoun@gmail.com>
2022-07-16 12:42:35 +0200

    no message

--- CV_socials/trying.py
+++ CV_socials/trying.py
@@ -1,21 +1,25 @@
 import cv2
 import numpy as np
-
-
 import time
-import pyautogui
+import pyautogui as p
+#viewLocator = max_loc
 
-time.sleep(5)
-##take screen on initial page, save, find view, get location, click
-myScreenshot = pyautogui.screenshot()
-myScreenshot.save('findView.png')
-screenshot = cv2.imread("findView.png", 0)
-template = cv2.imread("view2.png",  0)
-res = cv2.matchTemplate(screenshot, template, cv2.TM_SQDIFF)
-min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-viewLocator = min_loc
-time.sleep(2)
-pyautogui.click(viewLocator)
+
+def takeSS_findTemplateInScreen_clickOnTemplate():
+    time.sleep(5)
+    ##take screen on initial page, save, find view, get location, click
+    myScreenshot = p.screenshot()
+    myScreenshot.save('findView.png')
+    screenshot = cv2.imread("findView.png", 0)
+    template = cv2.imread("view4.png",  0)
+    res = cv2.matchTemplate(screenshot, template, cv2.TM_SQDIFF)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    viewLocator = min_loc
+
+    print(viewLocator)
+    print(cv2.minMaxLoc(res))
+    time.sleep(2)
+    p.click(viewLocator)
 
 
 
